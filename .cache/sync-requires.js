f85dd53377abf8bfa147421d5e0b2b2eefa3edb7
kimSpiegel04 <k.spiegel72@gmail.com>
2020-09-03 00:13:16 -0400

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\kspie\\Desktop\\50ReactProjects\\01-marketing-website\\.cache\\dev-404-page.js")))
+}
+
